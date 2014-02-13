bf2da6b1c5ab6caf63b09d39e5c38af6196e95b4
Kittinan Ponkaew <kponkaew@ucsd.edu>
2014-02-13 15:18:39 -0800

    finished

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,13 +26,33 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/"+idNumber, processProj);
+
 	console.log("User clicked on project " + idNumber);
 }
+function processProj(result){
+	console.log(result);
+	var projectHTML ="<img src='"+result['image'] + "' class='detailsImage'>"+
+	"<p>"+result['title']+'</p>'+'<p><small>'+result['date']+'</small></p><p>'+result['summary']+'</p>';
+	//$("#project-container").html(projectHTML);
+	//$("#project-description").html(result['summary']);
+	$("div #project"+result['id']+" .details").html(projectHTML);
 
+}
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", rc);
+}
+
+function rc(results){
+	var colors = results['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
